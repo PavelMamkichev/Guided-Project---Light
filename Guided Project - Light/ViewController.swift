bce79bcaf47b1d10b151f00c681cf7511fcba233
Pavel Mamkichev <lineage300089@gmail.com>
2020-04-02 17:26:22 +0300

    Initial Commit

---
+++ Guided Project - Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guided Project - Light
+//
+//  Created by Pavel Mamkichev on 02.04.2020.
+//  Copyright © 2020 Павел Мамкичев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
